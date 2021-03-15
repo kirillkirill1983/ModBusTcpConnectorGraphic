961c4a68b194ca7f23eb140caf614180c18060ae
baevkirill1983 <baevkirill1983@gmail.com>
2021-03-15 10:51:49 +0200

    Creat project v0.1

---
+++ RealTimeGraphX.WPF.Demo/MainWindowVM.cs
@@ -0,0 +1,67 @@
+﻿using EasyModbus;
+using RealTimeGraphX.DataPoints;
+using RealTimeGraphX.Renderers;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace RealTimeGraphX.WPF.Demo
+{
+    public class MainWindowVM
+    {
+        public WpfGraphController<TimeSpanDataPoint, DoubleDataPoint> Controller { get; set; }
+        //public WpfGraphController<DateTimeDataPoint, DoubleDataPoint> Controller { get; set; }
+
+
+        public MainWindowVM()
+        {
+            Controller = new WpfGraphController<TimeSpanDataPoint, DoubleDataPoint>();
+            //Controller = new WpfGraphController<DateTimeDataPoint, DoubleDataPoint>();
+            Controller.Range.MinimumY = 0;
+            Controller.Range.MaximumY = 33000;
+            Controller.Range.MaximumX = TimeSpan.FromSeconds(30);
+            //Controller.Range.MaximumX =DateTime.Now;
+            Controller.Range.AutoY = true;
+            Controller.Range.AutoYFallbackMode = GraphRangeAutoYFallBackMode.MinMax;
+
+            Controller.DataSeriesCollection.Add(new WpfGraphDataSeries()
+            {
+                Name = "Coonnect MB Device",
+                Stroke = Colors.DarkGreen,
+            });
+
+            
+
+            ModbusClient modbusClient;
+            modbusClient = new ModbusClient("192.168.79.56", 502);
+            //modbusClient = new ModbusClient("127.0.0.1", 502);
+            modbusClient.Connect();
+
+            Stopwatch watch = new Stopwatch();
+            
+            TimeSpan myData = DateTime.Now.TimeOfDay;
+            watch.Start();
+
+
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    int[] readHoldingRegisters = modbusClient.ReadHoldingRegisters (1539, 1);
+                    int y = readHoldingRegisters[0];
+                    double res; 
+                    res =0.01869 * y + 700;
+                    var x = (watch.Elapsed);
+                    TimeSpan newtime =myData+x;
+                    Controller.PushData(newtime, res);
+                    Thread.Sleep(10);
+                }
+            });
+        }
+    }
+}
---
+++ RealTimeGraphX.WPF.Demo/WpfGraphControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace RealTimeGraphX.WPF.Demo
+{
+    public class WpfGraphControl : Control
+    {
+        /// <summary>
+        /// Gets or sets the graph controller.
+        /// </summary>
+        public IGraphController Controller
+        {
+            get { return (IGraphController)GetValue(ControllerProperty); }
+            set { SetValue(ControllerProperty, value); }
+        }
+        public static readonly DependencyProperty ControllerProperty =
+            DependencyProperty.Register("Controller", typeof(IGraphController), typeof(WpfGraphControl), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Initializes the <see cref="WpfGraphControl"/> class.
+        /// </summary>
+        static WpfGraphControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WpfGraphControl), new FrameworkPropertyMetadata(typeof(WpfGraphControl)));
+        }
+    }
+}
